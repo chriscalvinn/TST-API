41e8126f5fe84b5829d68126e30d7a97e73813b9
calvin <calvinirianto@gmail.com>
2019-11-28 18:07:42 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,187 @@
+import pymysql
+from flaskext.mysql import MySQL
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from json import dumps
+import mysql.connector
+import json
+import requests
+
+app = Flask(__name__)
+mysql = MySQL(app)
+app.config['DEBUG'] = True
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'dbase'
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+mysql.init_app(app)
+
+def checkKey(dict, key):
+#mengembalikan true jika key ada di dictionary dan false jika tidak
+    if key in dict: 
+        return True
+    else: 
+        return False
+
+@app.route('/book', methods=['GET'])
+def index():
+    #mengambil dari database
+    global cursor, conn
+    if request.method == 'GET':
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+            judul=request.args.get('judul')
+            if (judul==None):
+                cursor.execute("select * from book")
+                res = cursor.fetchall()
+            else:
+                cursor.execute("select * from book where Judul=%s",judul)
+                res = cursor.fetchall()
+                
+        except Exception as e:
+            return e
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        res = [{
+            'status': 201,
+            'message': 'Failed',
+        }]
+        
+    return jsonify({'result': res})
+
+@app.route('/admin', methods=['POST','DELETE','PUT'])
+def change():
+    global cursor, conn
+    #mengupload ke database
+    if request.method == 'POST':
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+
+            judul = request.form['judul']
+            isbn = request.form['isbn']
+            penulis = request.form['penulis']
+            penerbit = request.form['penerbit']
+            tahun_terbit = request.form['tahun_terbit']
+            harga = request.form['Harga']
+
+
+            query = "INSERT INTO book(Judul, ISBN, Penulis, Penerbit, tahun_terbit, Harga) VALUES(%s,%s,%s,%s,%s,%d)"
+
+            data = (judul, isbn, penulis, penerbit, tahun_terbit, Harga)
+            cursor.execute(query, data)
+            conn.commit()
+            res = {
+                'message' : 'Success',
+                'data': {
+                    'Judul' : judul,
+                    'ISBN' : isbn,
+                    'Penulis' : penulis,
+                    'Penerbit' : penerbit,
+                    'Tahun_Terbit' : tahun_terbit,
+                    'Harga' : Harga,
+                    }
+                }
+
+        except Exception as e:
+            return e
+        finally:
+            cursor.close()
+            conn.close()
+
+    #menghapus dari database
+    elif request.method == 'DELETE':
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            data = request.args.get('judul')
+            query = """DELETE FROM book WHERE judul=%s"""
+            cursor.execute(query, data)
+            conn.commit()
+            res = [{
+            'status': 200,
+            'message': "Deleted"
+            }]
+            return jsonify(res)
+        except Exception as e:
+            return e
+        finally:
+            cursor.close()
+            conn.close()
+
+    #mengubah entri database
+    elif request.method == 'PUT':
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+
+            judul = request.form.get('judul')
+            isbn = request.form.get('isbn')
+            penulis = request.form.get('penulis')
+            penerbit = request.form.get('penerbit')
+            tahun_terbit = request.form.get('tahun_terbit')
+            harga = request.form.get('Harga')
+
+            query = "UPDATE book SET ISBN=%s, Penulis=%s, Penerbit=%s, tahun_terbit=%s, Harga=%s WHERE Judul=%s"
+            data = (isbn, penulis, penerbit, tahun_terbit, harga, judul)
+            cursor.execute(query,data)
+            conn.commit()
+
+            res = {
+                'message' : 'Success',
+                'data': {
+                    'Judul' : judul,
+                    'ISBN' : isbn,
+                    'Penulis' : penulis,
+                    'Penerbit' : penerbit,
+                    'tahun_terbit' : tahun_terbit,
+                    'Harga' : harga,
+                    }
+                }  
+        except Exception as e:
+            return e
+        finally:
+            cursor.close()
+            conn.close()
+            
+    else:
+        res = [{
+            'status': 201,
+            'message': 'Failed',
+        }]
+        
+    return jsonify({'result': res})
+
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
+@app.errorhandler(405)
+def not_found(error=None):
+    message = {
+        'status': 405,
+        'message': 'Method ' + request.method + ' is not allowed',
+    }
+    resp = jsonify(message)
+    resp.status_code = 405
+
+    return resp
+
+if __name__ == '__main__':
+    app.run()
---
+++ scrapper.py
@@ -0,0 +1,91 @@
+import requests
+import json
+import mysql.connector
+#random
+import random
+random.seed(5)
+
+
+conn =  mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="dbase"
+)
+
+def checkKey(dict, key):
+#mengembalikan true jika key ada di dictionary dan false jika tidak
+    if key in dict: 
+        return True
+    else: 
+        return False
+
+def isiDbase():
+    cursor = conn.cursor()
+    for i in range (137000, 137100):
+    #melakukan pencarian sesuai range yang ditetapkan
+        try :
+            url = 'https://webpac.lib.itb.ac.id/index.php/marc/view/{}/JSON'.format(i)
+
+            #memasukkan data JSON ke variabel data
+            data = requests.get(url)
+            data = json.loads(data.text)
+
+            #mencari Judul (Jika tidak ada diberi nilai null)
+            for j in range (0, len(data[0]['fields'])):
+                if checkKey(data[0]['fields'][j],'245') :
+                    Judul = data[0]['fields'][j]['245']['subfields'][0]['a']
+                    break
+                else :
+                    Judul = "null"
+
+            #mencari ISBN (Jika tidak ada diberi nilai null)
+            ISBN = random.randrange(0000000000000, 9999999999999)
+
+            #mencari Penulis (Jika tidak ada diberi nilai null)
+            for j in range (0, len(data[0]['fields'])):
+                if checkKey(data[0]['fields'][j],'100') :
+                    Penulis = data[0]['fields'][j]['100']['subfields'][0]['a']
+                    break
+                else :
+                    Penulis = "null"
+
+            #mencari Penerbit dan Tahun Terbit (Jika tidak ada diberi nilai null)
+            for j in range (0, len(data[0]['fields'])):
+                if checkKey(data[0]['fields'][j],'260') :
+                    Penerbit = data[0]['fields'][j]['260']['subfields'][1]['b']
+                    Tahun_terbit = data[0]['fields'][j]['260']['subfields'][2]['c']
+                    break
+                else :
+                    Penerbit = "null"
+                    Tahun_terbit = "null"
+
+            #Harga
+            Harga = random.randrange(100000,500000,1000)
+
+            #insert into database
+            instance = (ISBN, Judul, Penulis, Penerbit, Tahun_terbit, Harga)
+            query = """INSERT INTO book(
+                ISBN,
+                Judul,
+                Penulis,
+                Penerbit,
+                Tahun_Terbit,
+                Harga
+                )
+                Values (%s,%s,%s,%s,%s,%s)"""
+            cursor.execute(query, instance)
+            conn.commit()
+
+        #menghandle exception
+        except :
+            print()
+            continue
+        
+    cursor.close()
+    
+def main():
+	isiDbase()
+	
+
+main()
