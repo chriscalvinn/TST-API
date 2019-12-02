279c6b8bb3dc06e9c520393d329f4d15cddf7720
calvin <calvinirianto@gmail.com>
2019-12-02 17:19:18 +0700

    update script

--- js/search.js
+++ js/search.js
@@ -1,31 +1,42 @@
 
 
-const search = async() => {
-	const data = await JSON.stringify({
-		judul:document.getElementById("judul").value,
-	})
-	var url = 'http://1127.0.0.1/book?Judul=' +judul
-	const response = await fetch(url,{
-		method:'GET',
-		headers:{
-			'Content-Type':'application/json'
-		},
-	})
-	const json = await response.json();
-	console.log(json);
-	var content="";
-	json.forEach(function(element){
-		content +=
-			"<tr>" +
-			"<td><font color = "+'"white"'+">"+element.Judul+"</font></th>\n" +
-			"<td><font color = "+'"white"'+">"+element.Penulis+"</font></th>\n" +
-			"<td><font color = "+'"white"'+">"+element.Penerbit+"</font></th>\n" +
-			"<td><font color = "+'"white"'+">"+element.Tahun_Terbit+"</font></th>\n" +
-			"<td><font color = "+'"white"'+">"+element.Harga+"</font></th>\n" +
-			"<td><font color = "+'"white"'+">"+element.Berat+"</font></th>\n" +
-			"<td><button id=\"order\" Detail</button>" +
-			"</tr>";
-	});
+function search(){
 	
-	document.getElementById('tabbodybuku').innerHTML = content;
+	judul = document.getElementById("judul").value
+	if(judul!=''){
+		console.log(judul);
+		var uri = 'http://127.0.0.1:5001/book?judul='+judul
+	} else {
+		var uri = 'http://127.0.0.1:5001/book'
+	}
+	console.log(uri);
+	$(document).ready(function(){
+		$.ajax({
+			url: uri,
+			type:'GET',
+			dataType: 'json',
+			success: function(response) {
+				content="";
+				$.each(response, function(key, value){
+					value.forEach(function(element){
+						content +=
+							"<tr>" +
+							"<td><font color = "+'"white"'+">"+element.Judul+"</font></th>\n" +
+							"<td><font color = "+'"white"'+">"+element.Penulis+"</font></th>\n" +
+							"<td><font color = "+'"white"'+">"+element.Penerbit+"</font></th>\n" +
+							"<td><font color = "+'"white"'+">"+element.Tahun_Terbit+"</font></th>\n" +
+							"<td><font color = "+'"white"'+">"+element.Harga+"</font></th>\n" +
+							"<td><font color = "+'"white"'+">"+element.Berat+"</font></th>\n" +
+							"<td><a href=Total.html id=\"order\"> Detail</a>" +
+							"</tr>";
+						});
+				});
+				console.log(content);
+				document.getElementById('tabbodybuku').innerHTML = content;
+			},
+			error: function(response) {
+				console.log(response);
+			}
+		});
+	});
 }
